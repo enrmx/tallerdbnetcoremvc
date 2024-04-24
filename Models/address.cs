205b0a70406fafcf26198c9b92b35599759e28ec
Enrique Rodriguez <l20211652@tectijuana.edu.mx>
2024-04-24 12:29:00 -0700

    Add project files.

---
+++ Models/address.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Models
+{
+    public class address
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
